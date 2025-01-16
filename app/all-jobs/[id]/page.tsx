9fe5f1656438402c4e9e1198935a02734cacf759
subhash <subhashkumarsingh.101@gmail.com>
2025-01-16 22:28:11 +0530

    test__18

--- app/all-jobs/[id]/page.tsx
+++ app/all-jobs/[id]/page.tsx
@@ -1,5 +1,5 @@
-import { FaCalendarAlt, FaBuilding, FaLink } from 'react-icons/fa';
-import pool from '@/lib/db';
+//import { FaCalendarAlt, FaBuilding, FaLink } from 'react-icons/fa';
+//import pool from '@/lib/db';
 
 interface JobDetail {
   id: string;
