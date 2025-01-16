8d35124f60ff9dd1095238787af91f0de154cc83
subhash <subhashkumarsingh.101@gmail.com>
2025-01-16 20:35:40 +0530

    test 10

--- app/all-jobs/[id]/page.tsx
+++ app/all-jobs/[id]/page.tsx
@@ -29,7 +29,7 @@ interface JobDetail {
 }
 
 interface JobDetailPageProps {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 }
 
 export default async function JobDetail({ params }: JobDetailPageProps) {
