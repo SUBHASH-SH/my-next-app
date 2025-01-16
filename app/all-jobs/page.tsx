219cfd957889a24e19d63b572a9f239d2cdb71d5
subhash <subhashkumarsingh.101@gmail.com>
2025-01-16 20:19:47 +0530

    test8

--- app/all-jobs/page.tsx
+++ app/all-jobs/page.tsx
@@ -26,7 +26,7 @@ export default async function AllJobs() {
         {jobs.map((job) => {
           return (
             <li key={job.id}>
-              <Link href={`/all-pa/${job.id}`}>
+              <Link href={`/all-jobs/${job.id}`}>
                 <div style={{ cursor: 'pointer', padding: '10px', border: '1px solid #ccc', marginBottom: '10px' }}>
                   <h2>{job.title}</h2>
                   <p>Organization: {job.organization}</p>
